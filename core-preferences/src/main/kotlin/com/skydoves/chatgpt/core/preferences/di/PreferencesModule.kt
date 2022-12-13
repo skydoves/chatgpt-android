311465dcc9ebb15423a91fffde6ed994769ca828
skydoves <skydoves@naver.com>
2022-12-13 13:14:42 +0900

    Implement preferences relevant classes and provide them via di

---
+++ core-preferences/src/main/kotlin/com/skydoves/chatgpt/core/preferences/di/PreferencesModule.kt
@@ -0,0 +1,39 @@
+/*
+ * Designed and developed by 2022 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skydoves.chatgpt.core.preferences.di
+
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import com.skydoves.chatgpt.core.preferences.Preferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class PreferencesModule {
+
+  @Provides
+  @Singleton
+  fun providePreferences(@ApplicationContext context: Context): Preferences {
+    val sharedPreferences = context.getSharedPreferences("ChatGPT_Stream", MODE_PRIVATE)
+    return Preferences(sharedPreferences)
+  }
+}

03ab8f456a7b771f14478de4788340bc7d4e5e8f
skydoves <skydoves@naver.com>
2024-04-02 23:29:02 +0900

    Complete ChatGPTLogin

--- feature-login/src/main/kotlin/com/skydoves/chatgpt/feature/login/ChatGPTLogin.kt
+++ feature-login/src/main/kotlin/com/skydoves/chatgpt/feature/login/ChatGPTLogin.kt
@@ -16,23 +16,53 @@
 
 package com.skydoves.chatgpt.feature.login
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.skydoves.chatgpt.core.designsystem.theme.BACKGROUND900
+import com.skydoves.chatgpt.core.designsystem.theme.WHITE200
 import com.skydoves.chatgpt.core.navigation.AppComposeNavigator
 import com.skydoves.chatgpt.core.navigation.ChatGPTScreens
 import io.getstream.chat.android.client.ChatClient
-import io.getstream.chat.android.models.ConnectionState
-import kotlinx.coroutines.flow.collectLatest
+import io.getstream.chat.android.models.InitializationState
 
 @Composable
 fun ChatGPTLogin(
   composeNavigator: AppComposeNavigator
 ) {
-  LaunchedEffect(key1 = Unit) {
-    ChatClient.instance().clientState.connectionState.collectLatest {
-      if (it == ConnectionState.Connected) {
-        composeNavigator.navigateAndClearBackStack(ChatGPTScreens.Channels.name)
-      }
+  val initializationState
+    by ChatClient.instance().clientState.initializationState.collectAsStateWithLifecycle()
+
+  LaunchedEffect(key1 = initializationState) {
+    if (initializationState == InitializationState.COMPLETE) {
+      composeNavigator.navigateAndClearBackStack(ChatGPTScreens.Channels.name)
+    }
+  }
+
+  Box(
+    modifier = Modifier
+      .fillMaxSize()
+      .background(BACKGROUND900)
+  ) {
+    if (initializationState == InitializationState.INITIALIZING) {
+      CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+    } else if (initializationState == InitializationState.NOT_INITIALIZED) {
+      Text(
+        modifier = Modifier.padding(14.dp),
+        text = stringResource(id = R.string.error_chat_sdk_initialization),
+        color = WHITE200
+      )
     }
   }
 }

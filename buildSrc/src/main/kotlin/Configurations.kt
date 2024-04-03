a428d826525aa3e664c2bc9e3f7b8d6a10ba0c72
skydoves <skydoves@naver.com>
2024-04-03 10:59:24 +0900

    Prepare for the next release

--- buildSrc/src/main/kotlin/Configurations.kt
+++ buildSrc/src/main/kotlin/Configurations.kt
@@ -3,8 +3,8 @@ object Configurations {
   const val targetSdk = 34
   const val minSdk = 21
   const val majorVersion = 1
-  const val minorVersion = 0
-  const val patchVersion = 10
+  const val minorVersion = 1
+  const val patchVersion = 0
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 11
+  const val versionCode = 12
 }

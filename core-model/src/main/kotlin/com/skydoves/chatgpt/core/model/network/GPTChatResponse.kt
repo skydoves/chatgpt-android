fc6bcf7b22040facf8cb799dd101bb1e5da030df
skydoves <skydoves@naver.com>
2024-07-24 12:10:42 +0900

    Fix nullable response field

--- core-model/src/main/kotlin/com/skydoves/chatgpt/core/model/network/GPTChatResponse.kt
+++ core-model/src/main/kotlin/com/skydoves/chatgpt/core/model/network/GPTChatResponse.kt
@@ -27,7 +27,7 @@ data class GPTChatResponse(
   @field:Json(name = "object") val `object`: String,
   @field:Json(name = "created") val created: Long,
   @field:Json(name = "model") val model: String,
-  @field:Json(name = "system_fingerprint") val systemFingerprint: String,
+  @field:Json(name = "system_fingerprint") val systemFingerprint: String?,
   @field:Json(name = "choices") val choices: List<GPTChoice>,
   @field:Json(name = "usage") val usage: GPTUsage
 )
